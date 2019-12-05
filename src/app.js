7ba8cc4c9516f8e5a5f427d5123a037b3d39cc6b
github_username <azizkoura@hotmail.com>
2019-12-05 12:26:26 +0100

    show alert when post added

--- src/app.js
+++ src/app.js
@@ -5,10 +5,33 @@ import { ui } from './ui';
 document.addEventListener('DOMContentLoaded', getPosts);
 
 // Listen for add posts
+document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+// Get Posts
 function getPosts() {
   http
     .get('http://localhost:3000/posts')
     .then(data => ui.showPosts(data))
+    .catch(err => console.log(err));
+}
+
+// submit Post
+function submitPost() {
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+
+  const data = {
+    title,
+    body
+  };
+
+  // Create Post
+  http
+    .post('http://localhost:3000/posts', data)
+    .then(data => {
+      ui.showAlert('Post added', 'alert alert-success');
+      ui.clearFields();
+      getPosts();
+    })
     .catch(err => console.log(err));
 }
